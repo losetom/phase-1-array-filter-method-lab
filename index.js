ccc980a57dad1661c355534a4a65ee478cf7ee4a
Mike Loseto <losetom1998@gmail.com>
2022-07-23 14:40:04 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 // Code your solution here
-function findMatching() {
-    
+
+// RETURN A NEW ARRAY FILLED WITH VALUES 
+// WHAT EVER MAKE THE CALLBACK TRUE WILL BE ADDED TO 
+// THE NEW ARRAY 
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name) {
+    const callback = (driver) => {
+        return driver.toLowerCase() === name.toLowerCase()
+    }
+    const newArr = drivers.filter(callback)
+    return newArr
+}
+
+// const newArr = driversArr.filter((driver) => driver.toLowerCase() === name.toLowerCase())
+// return driver
+
+function fuzzyMatch(drivers, name) {
+    const callback = function(driver) {
+        return driver.startsWith(name);
+    }
+    const newArr = drivers.filter((driver) => driver.startsWith(name))
+    return newArr
+}
+
+function matchName(drivers, name){
+    const callback = (driverObj) => {
+        const driver = driverObj.name 
+        return driver === name 
+    }
+
+    return drivers.filter(callback)
 }
